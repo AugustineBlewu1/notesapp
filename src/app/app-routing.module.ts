8fc8d3a445252f584c1e1509cfd4cd3639b9fbc9
Augustine Blewudzi <40745784+AugustineBlewu1@users.noreply.github.com>
2022-11-17 20:29:47 +0000

    feat:notes app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { MainLayoutComponent } from './main-layout/main-layout.component';
+import { PageListComponent } from './page-list/page-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainLayoutComponent , children: [
+    { path: '', component: PageListComponent }
+  ] },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageListComponent } from './page-list/page-list.component';
+import { MainLayoutComponent } from './main-layout/main-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageListComponent,
+    MainLayoutComponent
   ],
   imports: [
     BrowserModule,
